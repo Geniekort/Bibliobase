6fe88e279d9db83dfa7a8b2094f232393ec9c9fe
Dave <geniekort@hotmail.com>
2021-05-04 19:31:15 +0200

    Nicer scrollbar

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -146,3 +146,25 @@ export default Vue.extend({
   }
 });
 </script>
+
+<style>
+/* custom scrollbar */
+::-webkit-scrollbar {
+  width: 20px;
+}
+
+::-webkit-scrollbar-track {
+  background-color: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #d6dee1;
+  border-radius: 10px;
+  border: 6px solid transparent;
+  background-clip: content-box;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: #a8bbbf;
+}
+</style>
